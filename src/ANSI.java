498a4d36ba3eaeca948d16b816537589f308668d
Stephanie M <shelovesgames965@gmail.com>
2025-11-22 17:41:17 -0500

    ansi colors!

---
+++ src/ANSI.java
@@ -0,0 +1,14 @@
+public class ANSI {
+
+    //here we hold const strings with ANSI codes to have a more colourful application!
+
+    //ESC[ octal is \033
+    //then you add the ANSI code for your text edit
+
+    public static final String DEFAULT = "\033[0m";
+    public static final String MAGENTA = "\033[35m";
+    public static final String RED = "\033[31m";
+    public static final String BLUE = "\033[34m";
+    public static final String YELLOW = "\033[33m";
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("GAME START");
-        }
+
+        System.out.println(ANSI.MAGENTA + "WELCOME TO MAGICAL GIRL BATTLE ARENA" + ANSI.DEFAULT);
+        System.out.println("Choose one of four Magical Girl to incarnate: ");
+        System.out.println(ANSI.BLUE + "Esme - Sharpshooter");
+        System.out.println(ANSI.RED + "Astrid - Jumbo Hammer");
+        System.out.println(ANSI.MAGENTA + "Iris - Dark Mage");
+        System.out.println(ANSI.YELLOW + "Evangeline - Light Mage");
+
+
+
+    }
     }
